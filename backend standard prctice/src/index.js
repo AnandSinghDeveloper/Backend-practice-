0b52f5e1077727d7978a70001aeb053ae04e3bc8
Anand Thakur <anandsingh23112005@gmail.com>
2024-12-14 12:54:35 +0530

    database prcatice

---
+++ backend standard prctice/src/index.js
@@ -0,0 +1,32 @@
+//  import mongoose from "mongoose";
+
+//  import express from "express";
+//   const App= express();
+
+
+
+
+
+//   ; ( async( )=>{
+
+//     try {
+//      await  mongoose.connect( "mongodb+srv://Anandsingh:<AnandThakurdatabase9654>@cluster0.ynmfk.mongodb.net");
+//       App.on("error",(error)=>{
+
+//         console.log("error express:",error)
+//       });
+        
+//     } catch (error) {
+
+//       App.listen(process.env.PORT,()=>{
+//         console.log(`app listening on port : ${process.env.PORT}`);
+        
+//       })
+
+//       console.error("database conction issue " ,error)
+//       throw error;
+      
+//     }
+
+
+//  })()
